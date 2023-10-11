ec8c81d8a73c815f2e7b533d989f74ff9cfa68fe
Васильев Егор <regenerationdream@gmail.com>
2023-10-11 20:11:44 +0300

    InitialCreate migration added

---
+++ Simbir.GO/Migrations/20231011170036_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Simbir.GO.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "varchar(25)", nullable: false),
+                    passwordhash = table.Column<string>(name: "password_hash", type: "text", nullable: false),
+                    refreshtoken = table.Column<string>(name: "refresh_token", type: "text", nullable: true),
+                    refreshtokenexptime = table.Column<DateTime>(name: "refresh_token_exp_time", type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_name",
+                table: "users",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
