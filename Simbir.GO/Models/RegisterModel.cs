dcf96b73d89f689de1cb5abba30fc8cd8894a23a
Васильев Егор <regenerationdream@gmail.com>
2023-10-18 11:53:23 +0300

    Models modified

--- Simbir.GO/Models/RegisterModel.cs
+++ Simbir.GO/Models/RegisterModel.cs
@@ -6,6 +6,10 @@ public class RegisterModel
 {
     [Required]
     public string Name { get; set; }
+    
     [Required]
+    [MinLength(5, ErrorMessage = "Пароль должен содержать минимум 5 символов")]
+    [MaxLength(15, ErrorMessage = "Пароль должен содержать максимум 15 символов")]
+    [RegularExpression(@"\d", ErrorMessage = "Пароль должен содержать хотя бы одну цифру")]
     public string Password { get; set; }
 }
--- Simbir.GO/Models/TokenModel.cs
+++ Simbir.GO/Models/TokenModel.cs
@@ -1,8 +1,4 @@
-﻿using System.Text;
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
-using Microsoft.Extensions.Primitives;
+﻿using System.Text.Json.Serialization;
 
 namespace Simbir.GO.Models;
 
